a18f46b9b12977f107bd2422041f7dabecf90894
rafsalcas1@alum.us.es <rafsalcas1@alum.us.es>
2020-03-27 18:53:02 +0100

    He actualizado Paciente, se guarda en base de datos.

--- altaPaciente.php
+++ altaPaciente.php
@@ -1,78 +1,55 @@
 <?php
 	session_start();
-	
-	$poblaciones=array('CD' => 'Cádiz', 'SV' => 'Sevilla', 'MA'=>'Málaga', 'HU' => 'Huelva', 'CO'=>'Córdoba', 
-					'JA'=>'Jaén', 'AL'=>'Almería', 'GR'=>'Granada', 'OT'=>'Otro');
 
-
+	require_once("gestionBD.php");
+	require_once("gestionarPaciente.php");
+		
 	// Comprobar que hemos llegado a esta página porque se ha rellenado el formulario
 	if (isset($_SESSION["formulario"])) {
 		$nuevoUsuario = $_SESSION["formulario"];
 		$_SESSION["formulario"] = null;
 		$_SESSION["errores"] = null;
-	} else 
+	}
+	else 
 		Header("Location: formularioPaciente.php");	
 
-	// Obtener el nombre completo de la población mediante un array
-	function getPoblacion($abreviatura){
-		global $poblaciones;
-		if(isset($poblaciones[$abreviatura])){
-			return $poblaciones[$abreviatura];
-		}else{
-			return "No existe la población". $abreviatura;
-	}}
+	$conexion = crearConexionBD(); 
 
-	// Función para formatear una fecha al formato de Oracle
-	function getFechaFormateada($fecha){
-		$fechaNacimiento = date('d/m/Y', strtotime($fecha));
-		return $fechaNacimiento;
-	}
-	
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
   <meta charset="utf-8">
-  <title>Gestión de biblioteca: Alta de Usuario</title>
+  <title>Gestión de Biblioteca: Alta de Usuario realizada con éxito</title>
 </head>
 
 <body>
-<?php
+	<?php
 		include_once("cabecera.php");
 	?>
-	<main>
-		<h1>Datos de  <?php echo $nuevoUsuario["nombre"]; ?></h1>
-			<div id="div_volver">	
-			   Pulsa <a href="formularioPaciente.php">aquí</a> para volver al formulario de altas de usuarios.
-			</div>
-			
-			<h2>El paciente <?php echo $nuevoUsuario["nombre"]; ?> ha sido dado de alta con éxito con los siguientes datos:</h2>
-			<ul>
-				<li><?php echo "NIF: " . $nuevoUsuario["nif"]; ?></li>
-				<li><?php echo "Nombre: " . $nuevoUsuario["nombre"]; ?></li>
-				<li><?php echo "Apellidos: " . $nuevoUsuario["apellidos"]; ?></li>
-				<li><?php echo "Correo: " . $nuevoUsuario["correo"]; ?></li>
-				<li><?php echo "Fecha de Nacimiento: " . getFechaFormateada($nuevoUsuario["fechaNacimiento"]); ?></li>
-				<li><?php echo "Fecha de Alta: " . getFechaFormateada($nuevoUsuario["fechaAlta"]); ?></li>
-				<li><?php echo "Direccion: " . $nuevoUsuario["direccion"]; ?></li>
-				<li><?php echo "Poblacion: " . getPoblacion($nuevoUsuario["poblacion"]);?></li>
-				<li><?php echo "Seguro: " . $nuevoUsuario["seguro"]; ?></li>
-				<li><?php
-				if($nuevoUsuario["nombreTutor"]!=""){	
-					echo "Nombre tutor: " . $nuevoUsuario["nombreTutor"];
-				}
-				;?></li>
-				<li><?php
-				if($nuevoUsuario["telefonoTutor"]!=""){	
-					echo "Telefono tutor: " . $nuevoUsuario["telefonoTutor"];
-				}
-				?></li>
-			</ul>
 
-</main>
-<?php
+	<main>
+		<?php if (alta_usuario($conexion, $nuevoUsuario)) {  
+		?>
+				<h1>Hola <?php echo $nuevoUsuario["nombre"]; ?>, gracias por registrarte</h1>
+				<div >	
+			   		Pulsa <a href="verPaciente.php">aquí</a> para acceder a la gestión de biblioteca.
+				</div>
+		<?php } else { ?>
+				<h1>El usuario ya existe en la base de datos.</h1>
+				<div >	
+					Pulsa <a href="formularioPaciente.php">aquí</a> para volver al formulario.
+				</div>
+		<?php } ?>
+
+	</main>
+
+	<?php
 		include_once("pie.php");
 	?>
 </body>
 </html>
+<?php
+	cerrarConexionBD($conexion);
+?>
--- formularioPaciente.php
+++ formularioPaciente.php
@@ -1,50 +1,42 @@
 <?php
-
 	session_start();
+
+	if (!isset($_SESSION['formulario'])) {
+		$formulario['nombre'] = "";
+		$formulario['apellidos'] = "";
+		$formulario['dni'] = "";
+		$formulario['fechaNacimiento'] = "";
+		$formulario['correo'] = "";
+		$formulario['poblacion'] = "";
+		$formulario['direccion'] = "";
+		$formulario['fechaAlta'] = "";
+		$formulario['seguro'] = "";
+		$formulario['nombreTutor'] = "";
+		$formulario['telefonoTutor'] = "";
 	
-	// Si no existen datos del formulario en la sesión, se crea una entrada con valores por defecto
-	if (!isset($_SESSION["formulario"])){
-		$formulario["nif"] = "";
-		$formulario["nombre"] = "";
-		$formulario["apellidos"] = "";
-		$formulario["correo"] = "";
-		$formulario["fechaNacimiento"] = "";
-		$formulario["poblacion"] = array();
-		$formulario["direccion"] = "";
-		$formulario["fechaAlta"] = "";
-		$formulario["seguro"] = "Publico";
-		$formulario["nombreTutor"] = "";
-		$formulario["telefonoTutor"] = "";
-		
-		
-		$_SESSION["formulario"] = $formulario;
-	}	
-	else {
-		// Si ya existían valores, los cogemos para inicializar el formulario
-		$formulario = $_SESSION["formulario"];
+		$_SESSION['formulario'] = $formulario;
 	}
-	
-	// Si hay errores de validación, hay que mostrarlos y marcar los campos
+	else
+		$formulario = $_SESSION['formulario'];
+			
 	if (isset($_SESSION["errores"]))
 		$errores = $_SESSION["errores"];
-	
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="css/estilo.css" />
-  <title>Formulario Paciente</title>
+  <title>Gestión de Pacientes: Alta de Pacientes</title>
 </head>
 
 <body>
-	
-	<?php 
-	include_once("cabecera.php");
+	<?php
+		include_once("cabecera.php");
 	?>
 	
-	<?php
-		// Mostrar los erroes de validación (Si los hay)
+	<?php 
 		if (isset($errores) && count($errores)>0) { 
 	    	echo "<div id=\"div_errores\" class=\"error\">";
 			echo "<h4> Errores en el formulario:</h4>";
@@ -53,132 +45,62 @@
   		}
 	?>
 	
-	<h2>Añadir Paciente</h2>
-	<form id="altaPaciente" method="post" action="validacionPaciente.php" novalidate>
-			<p><i>Los campos obligatorios están marcados con </i><em>*</em> por favor rellenelos</p>
-			<fieldset><legend><h2>Datos personales</h2></legend>
-				<div></div><label for="nif">NIF<em>*</em></label>
-				<input id="nif" name="nif" type="text" placeholder="12345678X" pattern="^[0-9]{8}[A-Z]" title="Ocho dígitos seguidos de una letra mayúscula" value="<?php echo $formulario['nif'];?>" required>
-				</div>
+	<form id="altaUsuario" method="get" action="validacionPaciente.php" novalidate>
+		<p><i>Los campos obligatorios están marcados con </i><em>*</em></p>
+		<fieldset><legend>Datos personales</legend>
+			<div></div><label for="dni">DNI<em>*</em></label>
+			<input id="dni" name="dni" type="text" placeholder="12345678X" pattern="^[0-9]{8}[A-Z]" title="Ocho dígitos seguidos de una letra mayúscula" value="<?php echo $formulario['dni'];?>" required>
+			</div>
 
-				<div><label for="nombre">Nombre:<em>*</em></label>
-				<input id="nombre" name="nombre" type="text" size="40" value="<?php echo $formulario['nombre'];?>" required/>
-				</div>
+			<div><label for="nombre">Nombre:<em>*</em></label>
+			<input id="nombre" name="nombre" type="text" size="40" value="<?php echo $formulario['nombre'];?>" required/>
+			</div>
 
-				<div><label for="apellidos">Apellidos:<em>*</em></label>
-				<input id="apellidos" name="apellidos" type="text" size="80" value="<?php echo $formulario['apellidos'];?>"/>
-				</div>
-
-				<div<<label for="fechaNacimiento">Fecha de nacimiento:<em>*</em></label>
-				<input type="date" id="fechaNacimiento" name="fechaNacimiento" value="<?php echo $formulario['fechaNacimiento'];?>"/>
-				</div>
-
-				<div><label for="correo">Email:<em>*</em></label>
-				<input id="correo" name="correo"  type="email" placeholder="usuario@dominio.extension" value="<?php echo $formulario['correo'];?>" required/><br>
-				</div>
-				
-				<div><label for="poblacion">Población:<em>*</em></label>
-				<select name="poblacion" id="poblacion" required>
-				  	<optgroup label="Andalucia">
-				  	<option value="CD" <?php      
-						if (in_array("CD",$formulario['poblacion'])){
-							echo "selected";
-						}?> >Cádiz</option>
-					<option value="SV"<?php      
-						if (in_array("SV",$formulario['poblacion'])){
-							echo "selected";
-						}?> >Sevilla</option>
-					<option value="MA"<?php      
-						if (in_array("MA",$formulario['poblacion'])){
-							echo "selected";
-						}?> >Málaga</option>
-					<option value="HU"<?php      
-						if (in_array("HU",$formulario['poblacion'])){
-							echo "selected";
-						}?> >Huelva</option>
-					<option value="CO"<?php      
-						if (in_array("CO",$formulario['poblacion'])){
-							echo "selected";
-						}?> >Córdoba</option>
-					<option value="JA"<?php      
-						if (in_array("JA",$formulario['poblacion'])){
-							echo "selected";
-						}?> >Jaén</option>
-					<option value="AL"<?php      
-						if (in_array("AL",$formulario['poblacion'])){
-							echo "selected";
-						}?> >Almería</option>
-					<option value="GR"<?php      
-						if (in_array("GR",$formulario['poblacion'])){
-							echo "selected";
-						}?> >Granada</option>
-					</optgroup>
-					<option value="OT"<?php      
-						if (in_array("OT",$formulario['poblacion'])){
-							echo "selected";
-						}?> >Otra</option>
-				</select>
-				</div>
-				
-				<div><label for="direccion">Direccion:<em>*</em></label>
-				<input id="direccion" name="direccion" type="text" size="80" value="<?php echo $formulario['direccion'];?>" required/>
-				</div>
-				</fieldset>
-				
-				<fieldset><legend><h2>Datos clínicos</h2></legend>
-				<div><label for="fechaAlta">Fecha Alta:<em>*</em></label>
-				<input type="date" id="fechaAlta" name="fechaAlta" value="<?php echo $formulario['fechaAlta'];?>" required/>
-				</div>
-				
-				<div><label>Seguro:<em>*</em></label>
-				<br>
-				<label>
-					<input name="seguro" type="radio" value="Privado" <?php if($formulario['seguro']=='Privado') echo ' checked ';?>/>
-					Privado</label>
-				<label>
-					<input name="seguro" type="radio" value="Publico" <?php if($formulario['seguro']=='Publico') echo ' checked ';?>/>
-					Público</label>
-				</div>
-				</fieldset>
-				
-				<fieldset><legend><h2>Datos tutor</h2></legend>
-				
-				<div>Los siguientes datos solo son necesarios si el paciente es menor de edad</div>
-				
-				<div><label for="nombreTutor">Nombre Tutor:</label>
-				<input id="nombreTutor" name="nombreTutor" type="text" value="<?php echo $formulario['nombreTutor'];?>"/>
-				</div>
-				
-				<div><label for="telefonoTutor">Teléfono Tutor:</label>
-				<input id="telefonoTutor" name="telefonoTutor" type="tel" value="<?php echo $formulario['telefonoTutor'];?>"/>
-				</div>
-				</fieldset>
-				
-				<fieldset><legend><h2>Datos de usuario</h2></legend>
-				<div><label for="nick">Nickname:<em>*</em></label>
-				<input id="nick" name="nick" type="text" size="40"/>
-				</div>
-
-				<div><label for="pass">Password:<em>*</em></label>
-				<input type="password" name="pass" id="pass" placeholder="Mínimo 8 caracteres entre letras y dígitos" required/>
-				</div>
-				
-				<div><label for="confirmpass">Confirmar Password:<em>*</em></label>
-				<input type="password" name="confirmpass" id="confirmpass" placeholder="Confirmación de contraseña" required />
-				</div>
-				</fieldset>	
+			<div><label for="apellidos">Apellidos:</label>
+			<input id="apellidos" name="apellidos" type="text" size="80" value="<?php echo $formulario['apellidos'];?>"/>
+			</div>
 			
-				<br>
-				<button type="submit"><img src="images/botonOkey.png" width="20" height="20"></button>
-				<button type="button"><img src="images/cancelButton.png" width="20" height="20"></button>
-				<br>
-				
-		</form>
+			<div<<label for="fechaNacimiento">Fecha de nacimiento:</label>
+			<input type="date" id="fechaNacimiento" name="fechaNacimiento" value="<?php echo $formulario['fechaNacimiento'];?>"/>
+			</div>
+
+			<div><label for="correo">Correo:<em>*</em></label>
+			<input id="correo" name="correo"  type="correo" placeholder="usuario@dominio.extension" value="<?php echo $formulario['correo'];?>" required/><br>
+			</div>
+			
+			<div><label for="poblacion">Poblacion:</label>
+			<input id="poblacion" name="poblacion" type="text" size="80" value="<?php echo $formulario['poblacion'];?>"/>
+			</div>
+			
+			<div><label for="direccion">Direccion:</label>
+			<input id="direccion" name="direccion" type="text" size="80" value="<?php echo $formulario['direccion'];?>"/>
+			</div>
+			
+			<div><label for="fechaAlta">Fecha Alta:</label>
+			<input id="fechaAlta" name="fechaAlta" type="date" size="80" value="<?php echo $formulario['fechaAlta'];?>"/>
+			</div>
+			
+			<div><label for="seguro">Seguro:</label>
+			<input id="seguro" name="seguro" type="text" size="80" value="<?php echo $formulario['seguro'];?>"/>
+			</div>
+			
+			<div><label for="nombreTutor">Nombre Tutor:</label>
+			<input id="nombreTutor" name="nombreTutor" type="text" size="80" value="<?php echo $formulario['nombreTutor'];?>"/>
+			</div>
+			
+			<div><label for="telefonoTutor">Telefono Tutor:</label>
+			<input id="telefonoTutor" name="telefonoTutor" type="text" size="80" value="<?php echo $formulario['telefonoTutor'];?>"/>
+			</div>
+		</fieldset>
+
+		<div><input type="submit" value="Enviar" /></div>
+
+	</form>
 	
-	<?php 
+	<?php
 		include_once("pie.php");
 	?>
 	
 	</body>
-
 </html>
+
