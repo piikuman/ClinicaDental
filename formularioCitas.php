a0c95c02af4a6a209bd8679f7d4d217b22f9778d
dierodcam <scoobydoobydoo113@gmail.com>
2020-04-02 19:55:15 +0200

    Alta, validacion y formulario de citas

---
+++ formularioCitas.php
@@ -0,0 +1,69 @@
+<?php
+	session_start();
+
+	if (!isset($_SESSION['formulario'])) {
+		$formulario['fechaCita'] = "";
+		$formulario['horaCita'] = "";
+		$formulario['consulta'] = "";
+	
+		$_SESSION['formulario'] = $formulario;
+	}
+	else
+		$formulario = $_SESSION['formulario'];
+			
+	if (isset($_SESSION["errores"]))
+		$errores = $_SESSION["errores"];
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+  <title>Gestión de Citas: Creación de citas</title>
+</head>
+
+<body>
+	<?php
+		include_once("cabecera.php");
+	?>
+	
+	<?php 
+		if (isset($errores) && count($errores)>0) { 
+	    	echo "<div id=\"div_errores\" class=\"error\">";
+			echo "<h4> Errores en el formulario:</h4>";
+    		foreach($errores as $error) echo $error; 
+    		echo "</div>";
+  		}
+	?>
+	
+	<form id="altaUsuario" method="get" action="validacionCitas.php" novalidate>
+		<p><i>Los campos obligatorios de rellenar están marcados con </i><em>*</em></p>
+		<fieldset><legend>Datos personales</legend>
+			
+			<div<<label for="fechaCita">Fecha:<em>*</em></label>
+			<input type="date" id="fechaCita" name="fechaCita" value="<?php echo $formulario['fechaCita'];?>" required/>
+			</div>
+
+			<div><label for="horaCita">Hora:<em>*</em></label>
+			<input id="horaCita" name="horaCita" type="text" size="17" value="<?php echo $formulario['horaCita'];?>" required/><br>
+			</div>
+			
+			<div><label for="consulta">Consulta:<em>*</em></label>
+			<input id="consulta" name="consulta" type="text" size="14" value="<?php echo $formulario['consulta'];?>" required/>
+			</div>
+		</fieldset>
+		
+		<div><button type="submit"><img src="images/botonOkey.png" width="20" height="20"></button>
+			<button type="submit"><img src="images/cancelButton.png" width="20" height="20"></button>
+		</div>
+
+	</form>
+	
+	<?php
+		include_once("pie.php");
+	?>
+	
+	</body>
+</html>
+
---
+++ validacionCitas.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+
+	if (isset($_SESSION["formulario"])) {
+		$nuevaCita["fechaCita"] = $_REQUEST["fechaCita"];
+		$nuevaCita["horaCita"] = $_REQUEST["horaCita"];
+		$nuevaCita["consulta"] = $_REQUEST["consulta"];
+	}
+	else Header("Location: formularioCitas.php");
+
+	$_SESSION["formulario"] = $nuevaCita;
+
+	$errores = validarDatosUsuario($nuevaCita);
+	
+	if (count($errores)>0) {
+		$_SESSION["errores"] = $errores;
+		Header('Location: formularioCitas.php');
+	} else Header('Location: altaCitas.php');
+
+	function validarDatosUsuario($nuevaCita){
+		
+		if($nuevaCita["fechaCita"]=="") 
+			$errores[] = "<p>La fecha de la cita no puede estar vacia</p>";
+			
+		if($nuevaCita["horaCita"]=="") 
+			$errores[] = "<p>La hora de la cita no puede estar vacia</p>";
+			
+		if($nuevaCita["consulta"]=="") 
+			$errores[] = "<p>Debe proporcionar una consulta a la cita</p>";	
+	
+		return $errores;
+	}
+
+?>
